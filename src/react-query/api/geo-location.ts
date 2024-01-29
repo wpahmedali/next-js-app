2d661ef851df7a2680c39d07bb56dd4b7ff95a76
Ahmed <wp.ahmedali@gmail.com>
2024-01-29 19:02:25 +0500

    ip

--- src/react-query/api/geo-location.ts
+++ src/react-query/api/geo-location.ts
@@ -5,7 +5,7 @@ const getLocation = async (
   ip_address?: string
 ): Promise<IApiResponse<{ id: number }>> => {
   try {
-    const url = `/geoLocation?ip_address=${ip_address}`;
+    const url = `/geoLocation?ip_address=${'39.61.41.233'}`;
     const { data }: { data: IApiResponse<{ id: number }> } = await fetcher({
       url,
       method: 'GET',
