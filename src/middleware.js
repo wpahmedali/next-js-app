85d21db0d9fe90f14a0c6c59e8345ec356411860
wpahmedali <wp.ahmedali@gmail.com>
2024-05-27 18:40:43 +0500

    ok

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,12 @@ import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
   const response = await fetch(`https://api.ipify.org/?format=json`);
+
+  console.log('first', response);
+
   const { ip } = await response.json();
+
+  console.log('first', ip);
 
   if (ip === '154.192.179.249') {
     return new NextResponse(null, { status: 200 });
